49f02c98c2c2defb0a7a2e51fae7b6bd32817161
Nameless <dev@name1ess.ga>
2022-04-05 21:24:40 +0100

    Update UnlockAllLand-Looped.lua

--- UnlockAllLand-Looped.lua
+++ UnlockAllLand-Looped.lua
@@ -1,11 +1,10 @@
-local function unlockLand(player)
-    for _, land in next, workspace["Star_"..player.Name].Player.Land:GetChildren() do
-        game.ReplicatedStorage.PlayerData.Unlocker.LandShow:FireServer(land)
+local function unlockLand()
+    for _, player in next, game.Players:GetChildren() do
+        for _, land in next, workspace["Star_"..player.Name].Player.Land:GetChildren() do
+            game.ReplicatedStorage.PlayerData.Unlocker.LandShow:FireServer(land)
+        end
     end
 end
 
-for _, player in next, game.Players:GetChildren() do
-    unlockLand(player)
-end
-
 game.Players.PlayerAdded:Connect(unlockLand)
+unlockLand()
