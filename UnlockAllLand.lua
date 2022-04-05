77148c2acaf42a3d9086b2e1fe3fa6bdb65c58bb
Nameless <dev@name1ess.ga>
2022-04-05 21:18:08 +0100

    Create UnlockAllLand.lua

---
+++ UnlockAllLand.lua
@@ -0,0 +1,5 @@
+for _, player in next, game.Players:GetChildren() do
+    for _, land in next, workspace["Star_"..player.Name].Player.Land:GetChildren() do
+        game.ReplicatedStorage.PlayerData.Unlocker.LandShow:FireServer(land)
+    end
+end
